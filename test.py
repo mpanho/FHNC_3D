29bced34dade6c09f025f7302c4107f865a4701c
mpanho <36071700+mpanho@users.noreply.github.com>
2018-06-22 13:18:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,11 @@
 import FHNC_3D as FHNC
 import numpy as np
 
+rs=input('Enter rs vaule:')
 FHNC.set_default()
 FHNC.nu=2
 FHNC.update()
-gr=FHNC.solve_EL(10.)
+gr=FHNC.solve_EL(rs)
 
 tmp=np.array([FHNC.x,gr])
 np.savetxt("result.txt",tmp.transpose(),fmt='%1.5e',header='#r  g(r)')
