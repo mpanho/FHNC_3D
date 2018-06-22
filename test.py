36bf182390f59b94823c338bd32994c7c9043f9e
mpanho <36071700+mpanho@users.noreply.github.com>
2018-06-22 13:49:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,14 @@ import numpy as np
 
 rs=input('Enter rs vaule:')
 FHNC.set_default()
-FHNC.nu=2
-FHNC.update()
+
+pol=input('for spin polarized calculation enter [y]:')
+if (pol=='y'):
+  FHNC.nu=1
+  FHNC.update()
+
 gr=FHNC.solve_EL(rs)
 
 tmp=np.array([FHNC.x,gr])
-np.savetxt("result.txt",tmp.transpose(),fmt='%1.5e',header='#r  g(r)')
+np.savetxt("result.txt",tmp.transpose(),fmt='%1.5e',header='#r/rs/a0  g(r) ')
+print('Output written to: result.txt')
